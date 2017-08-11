3077a6fb73e1e53c3645c9591e5d3c13ee641a77
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2017-08-11 18:59:44 +0200

    Texture flip fix

--- app/Draw/Three/Shaders.ts
+++ app/Draw/Three/Shaders.ts
@@ -6,7 +6,7 @@ class ThreeJSShaders
         varying vec2 vUv;
         void main()
         {
-            vUv  = uv;
+            vUv  = vec2(1.0 - uv.x, uv.y);
 			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
         }
         `;
