3949082b478d467bc4c02ee162ed664827164fab
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2017-09-10 17:37:02 +0200

    Build changes [0.0.17]

--- app/Engine/Settings.ts
+++ app/Engine/Settings.ts
@@ -8,7 +8,7 @@ enum Quality
 }
 class Settings
 {
-    public static Version:string = "0.0.16";
+    public static Version:string = "0.0.17";
     public static LibPath:string = "/resources/";
     public static Graphics:Quality = Quality.High;
 }
