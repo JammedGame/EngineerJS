501b8b452254070e8b25762f9df6b2a1cf41c857
Unknown <manojlovicsmilos@hotmail.com>
2017-10-06 22:24:05 +0200

    Build Changes [0.0.19]

--- app/Engine/Settings.ts
+++ app/Engine/Settings.ts
@@ -8,7 +8,7 @@ enum Quality
 }
 class Settings
 {
-    public static Version:string = "0.0.18";
+    public static Version:string = "0.0.19";
     public static LibPath:string = "/resources/";
     public static Graphics:Quality = Quality.High;
 }
