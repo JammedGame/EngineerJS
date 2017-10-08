6873f803a943fedc520c924ee96314f1f602f536
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2017-10-08 03:20:52 +0200

    Build Changes [0.0.26]

--- app/Engine/Settings.ts
+++ app/Engine/Settings.ts
@@ -8,7 +8,7 @@ enum Quality
 }
 class Settings
 {
-    public static Version:string = "0.0.25";
+    public static Version:string = "0.0.26";
     public static LibPath:string = "/resources/";
     public static Graphics:Quality = Quality.High;
 }
