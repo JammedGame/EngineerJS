ab19813f024a22e0ddfeb26a3b82f9890f72640c
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2017-10-20 22:50:00 +0200

    Test Build [0.0.31.0]

--- app/Engine/Settings.ts
+++ app/Engine/Settings.ts
@@ -8,7 +8,7 @@ enum Quality
 }
 class Settings
 {
-    public static Version:string = "0.0.15";
+    public static Version:string = "Test 0.31.0";
     public static LibPath:string = "/resources/";
     public static Graphics:Quality = Quality.High;
 }
